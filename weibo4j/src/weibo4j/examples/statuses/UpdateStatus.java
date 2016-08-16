76674bad029b29319f30c2a1f913cb1aa8252d2b
Michealpan9999 <1174925395@qq.com>
2016-08-17 01:11:50 +0800

    first commit

---
+++ weibo4j/src/weibo4j/examples/statuses/UpdateStatus.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package weibo4j.examples.statuses;
+
+import weibo4j.Status;
+import weibo4j.Weibo;
+
+/**
+ * @author sina
+ *
+ */
+public class UpdateStatus {
+
+	/**
+	 * 发布一条微博信息 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.setProperty("weibo4j.oauth.consumerKey", Weibo.CONSUMER_KEY);
+    	System.setProperty("weibo4j.oauth.consumerSecret", Weibo.CONSUMER_SECRET);
+        try {
+        	//创建一个微博对象
+        	//false base认证
+        	//true  oauth认证
+        	args = new String[]{"1073298017@qq.com","pzq19900506"};
+        	Weibo weibo = getWeibo(false,args);
+        	Status status = weibo.updateStatus("开发微博练习开始啦 ！！！");
+        	System.out.println(status.getId() + " : "+ status.getText()+"  "+status.getCreatedAt());
+        	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static Weibo getWeibo(boolean isOauth,String ... args) {
+		Weibo weibo = new Weibo();
+		if(isOauth) {//oauth验证方式 args[0]:访问的token；args[1]:访问的密匙
+			weibo.setToken(args[0], args[1]);
+		}else {//用户登录方式
+    		weibo.setUserId(args[0]);//用户名/ID
+    		weibo.setPassword(args[1]);//密码
+		}
+		return weibo;
+	}
+}
